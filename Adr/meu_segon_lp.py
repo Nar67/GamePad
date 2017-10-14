b8ca5b46017de9a9325ac7b5bab26aafed6043c7
Piltra <amartinezm1994@gmail.com>
2017-10-14 06:24:00 +0200

    arreglat6

--- Adr/meu_segon_lp.py
+++ Adr/meu_segon_lp.py
@@ -7,11 +7,11 @@ def check_if_out_of_bounds(x, y, YMIN, YMAX, XMIN, XMAX):
 	return x > XMAX or x < XMIN or y > YMAX or y < YMIN
 
 
-#lp = launchpad_py.LaunchpadMk2()
+lp = launchpad_py.LaunchpadMk2()
 
 
-#res = lp.Open()
-#lp.Reset()
+res = lp.Open()
+lp.Reset()
 
 XMIN = 0
 YMIN = 0
@@ -44,7 +44,7 @@ while 1:
 
 	matrix = [[]]
 
-	#ll = lp.ButtonStateXY()
+	ll = lp.ButtonStateXY()
 
 	for x in range(XMIN - 1 , XMAX + dist_marge*2 + 1):
 		submat = []
@@ -68,19 +68,19 @@ while 1:
 	for x in range(XMIN - 1, XMAX + 1):
 		for y in range(YMIN - 1, YMAX + 1):
 			matrix[x + dist_marge][y + dist_marge] = "*"
-			#lp.LedCtrlXYByCode(x, y, colorfons)
+			lp.LedCtrlXYByCode(x, y, colorfons)
 
 	suplist = []
 	for i, coord in enumerate(objects):
 		matrix[coord[0] + dist_marge][coord[1] + dist_marge] = "."
-		#lp.LedCtrlXYByCode(coord[0], coord[1], colorobjs)
+		lp.LedCtrlXYByCode(coord[0], coord[1], colorobjs)
 		coord[0] += desplacx
 		coord[1] += desplacy
 		if not check_if_out_of_bounds(coord[0], coord[1], YMIN - dist_marge/2, YMAX + dist_marge/2, XMIN - dist_marge/2, XMAX + dist_marge/2):
 			suplist.append([coord[0], coord[1]])
 	objects = suplist
 	matrix[xcoord + dist_marge][ycoord + dist_marge] = "+"
-	#lp.LedCtrlXYByCode(xcoord, ycoord, colorpers)
+	lp.LedCtrlXYByCode(xcoord, ycoord, colorpers)
 
 	if curr_torn%torns_gen == 0:
 		objects = objects + objs.get_obj_1(random.randint(XMIN - dist_marge/2, XMAX + dist_marge/2)*desplacy - desplacx*2, random.randint(YMIN - dist_marge/2, YMAX + dist_marge/2)*desplacx - desplacy*2)
@@ -90,16 +90,13 @@ while 1:
 
 	curr_torn += 1
 
+	'''
 	print "-------------------------------"
 
 	for x in matrix:
 		stringa = ""
 		for y in x:
 			stringa += y
-		print stringa
-
-
-	'''print('\n'.join([''.join(['{:4}'.format(item) for item in row]) 
-      for row in matrix]))'''
+		print stringa'''
 
 
