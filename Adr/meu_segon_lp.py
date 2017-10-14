2cf78fac5f91bb98fceba5c4f4835b8e4ae1d81e
Piltra <amartinezm1994@gmail.com>
2017-10-14 10:48:59 +0200

    arreglat6

--- Adr/meu_segon_lp.py
+++ Adr/meu_segon_lp.py
@@ -2,8 +2,16 @@ import launchpad_py
 import time
 import objs
 import random
+import signal
+import sys
 #import button_thread
 from threading import Lock, Thread
+
+def check_if_collide(points, pj):
+	for x in points:
+		if x[0] == pj[0] and x[1] == pj[1]:
+			return True
+
 
 def check_if_out_of_bounds(x, y, YMIN, YMAX, XMIN, XMAX):
 	return x > XMAX or x < XMIN or y > YMAX or y < YMIN
@@ -11,12 +19,15 @@ def check_if_out_of_bounds(x, y, YMIN, YMAX, XMIN, XMAX):
 class button_thread(Thread):
 
 	def __init__(self, execute_lock):
+	#def __init__(self, launchpad, execute_lock):
 		super(button_thread, self).__init__()
 		self.el = execute_lock
+		#self.lh = launchpad
 		self.flag = 0
 
-	def stopt(self):
+	def stopt(self, signum, frame):
 		self.flag = 1
+		sys.exit()
 
 	def run(self):
 		global ycoord
@@ -26,35 +37,43 @@ class button_thread(Thread):
 		global XMIN
 		global XMAX
 		while not self.flag:
+
 			ll = []
 
 			self.el.acquire()
-			ll = self.lh.ButtonStateXY()
+			#ll = self.lh.ButtonStateXY()
 			self.el.release()
 
-			#global ycoord
-			#ycoord = ycoord + 1
 
 
 			if(len(ll) != 0):
-				if ll[0] == 0 and ll[1] == 0 and ycoord < YMAX: # up
-					ycoord += 1
-
-				elif ll[0] == 1 and ll[1] == 0 and ycoord > YMIN: #down
+				if ll[0] == 0 and ll[1] == 0 and ycoord > YMIN: # up
+					self.el.acquire()
 					ycoord -= 1
+					self.el.release()
+
+				elif ll[0] == 1 and ll[1] == 0 and ycoord < YMAX: #down
+					self.el.acquire()
+					ycoord += 1
+					self.el.release()
 
 				elif ll[0] == 2 and ll[1] == 0 and xcoord > XMIN: # izquierda
+					self.el.acquire()
 					xcoord -= 1
+					self.el.release()
 
 				elif ll[0] == 3 and ll[1] == 0 and xcoord < XMAX: # derecha
+					self.el.acquire()
 					xcoord += 1
+					self.el.release()
 
 
-lp = launchpad_py.LaunchpadMk2()
+
+#lp = launchpad_py.LaunchpadMk2()
 
 
-res = lp.Open()
-lp.Reset()
+#res = lp.Open()
+#lp.Reset()
 
 XMIN = 0
 YMIN = 1
@@ -81,17 +100,20 @@ dist_marge = 5
 matrix = [[]]
 
 el = Lock()
-thr1 = button_thread(lp, el)
+#thr1 = button_thread(lp, el)
+thr1 = button_thread(el)
 thr1.start()
 
-
+signal.signal(signal.SIGINT, thr1.stopt)
+signal.signal(signal.SIGTERM, thr1.stopt)
+#signal.signal(signal.CTRL_C_EVENT, thr1.stopt)
 
 while 1:
-	#ll = []
+	ll = []
 
 	matrix = [[]]
 
-	ll = lp.ButtonStateXY()
+	#ll = lp.ButtonStateXY()
 
 	for x in range(XMIN - 1 , XMAX + dist_marge*2 + 1):
 		submat = []
@@ -101,36 +123,43 @@ while 1:
 
 	
 
-	for x in range(XMIN - 1, XMAX + 1):
-		for y in range(YMIN - 1, YMAX + 1):
+	for x in range(XMIN, XMAX + 1):
+		for y in range(YMIN, YMAX + 1):
 			if x != 8 and y != 0:
 				matrix[x + dist_marge][y + dist_marge] = "*"
 				el.acquire()
-				lp.LedCtrlXYByCode(x, y, colorfons)
+				#lp.LedCtrlXYByCode(x, y, colorfons)
 				el.release()
 
 	el.acquire()
-	lp.LedCtrlXYByCode(0, 0, 67)
-	lp.LedCtrlXYByCode(1, 0, 67)
-	lp.LedCtrlXYByCode(2, 0, 67)
-	lp.LedCtrlXYByCode(3, 0, 67)
+	#lp.LedCtrlXYByCode(0, 0, 67)
+	#lp.LedCtrlXYByCode(1, 0, 67)
+	#lp.LedCtrlXYByCode(2, 0, 67)
+	#lp.LedCtrlXYByCode(3, 0, 67)
 	el.release()
 
 	suplist = []
-	for i, coord in enumerate(objects):
+	for coord in objects:
+
 		matrix[coord[0] + dist_marge][coord[1] + dist_marge] = "."
+
 		if coord[0] != 8 and coord[1] != 0:
+
 			el.acquire()
-			lp.LedCtrlXYByCode(coord[0], coord[1], colorobjs)
+			#lp.LedCtrlXYByCode(coord[0], coord[1], colorobjs)
 			el.release()
+
 		coord[0] += desplacx
 		coord[1] += desplacy
-		if not check_if_out_of_bounds(coord[0], coord[1], YMIN - dist_marge/2, YMAX + dist_marge/2, XMIN - dist_marge/2, XMAX + dist_marge/2):
+		if not check_if_out_of_bounds(coord[0], coord[1], YMIN - dist_marge, YMAX + dist_marge, XMIN - dist_marge, XMAX + dist_marge):
 			suplist.append([coord[0], coord[1]])
+
 	objects = suplist
+
 	matrix[xcoord + dist_marge][ycoord + dist_marge] = "+"
+
 	el.acquire()
-	lp.LedCtrlXYByCode(xcoord, ycoord, colorpers)
+	#lp.LedCtrlXYByCode(xcoord, ycoord, colorpers)
 	el.release()
 
 	if curr_torn%torns_gen == 0:
@@ -141,13 +170,13 @@ while 1:
 
 	curr_torn += 1
 
-	'''
+	
 	print "-------------------------------"
 
 	for x in matrix:
 		stringa = ""
 		for y in x:
 			stringa += y
-		print stringa'''
+		print stringa
 
 
