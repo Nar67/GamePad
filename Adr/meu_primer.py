163bc6face9641549b22e7dcabc3a4bb44336b8f
Nar67 <narcis.tg@gmail.com>
2017-10-14 03:34:15 +0200

    added test function to test buttonstate

--- Adr/meu_primer.py
+++ Adr/meu_primer.py
@@ -8,8 +8,7 @@ res = lp.Open()
 lp.Reset()
 while 1:
 	ll = lp.ButtonStateXY()
-	if len(ll) == 0:
-		pass
-	lp.LedCtrlXYByCode(ll[0], ll[1], ll[2])
-	time.sleep(0.5)
-	lp.LedCtrlXYByCode(ll[0], ll[1], 0)
+	if len(ll) != 0:
+		lp.LedCtrlXYByCode(ll[0], ll[1], ll[2])
+		time.sleep(0.5)
+		lp.LedCtrlXYByCode(ll[0], ll[1], 0)
