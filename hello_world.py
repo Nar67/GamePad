ce370a94cc9fa4f2acd0ec9dcf2cdda26fe43c7a
Nar67 <narcis.tg@gmail.com>
2017-10-14 03:39:11 +0200

    function to test button state combined with a for

--- hello_world.py
+++ hello_world.py
@@ -4,8 +4,15 @@ lp = launchpad_py.LaunchpadMk2()
 
 res = lp.Open()
 lp.Reset()
-for x in range(11, 98):
-	lp.LedCtrlRawByCode(x, 72)
-	time.sleep(0.05)
-	lp.LedCtrlRawByCode(x, 0)
+while 1:
+	ll = lp.ButtonStateXY()
+	if(len(ll) != 0):
+		if(ll[0] == 0 and ll[1] == 8):
+			for x in range(11, 98):
+				lp.LedCtrlRawByCode(x, 23)
+				time.sleep(0.05)
+				lp.LedCtrlRawByCode(x, 0)
+	lp.ButtonFlush()
+
+	
 print(res)
